b6614d17bd898f42a6fecb539a876aaaaf3dfce5
ekuntsevich <ifk88@mail.ru>
2019-04-10 21:03:16 +0300

    updated logic for saving final file

--- src/main/java/svg/RedrawingSVG.java
+++ src/main/java/svg/RedrawingSVG.java
@@ -1,5 +1,6 @@
 package svg;
 
+import drawing.DrawText;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.apache.batik.svggen.SVGGraphics2DIOException;
 import org.apache.batik.swing.JSVGCanvas;
@@ -10,26 +11,32 @@ import org.w3c.dom.Element;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.net.MalformedURLException;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static drawing.DrawArrow.drawArrow;
 
 public class RedrawingSVG extends JFrame {
 
     protected AtomicBoolean shown = new AtomicBoolean(false);
 
-    public static void main(String[] args) throws MalformedURLException, InterruptedException, FileNotFoundException, UnsupportedEncodingException, SVGGraphics2DIOException {
+    public static void main(String[] args) throws MalformedURLException {
         RedrawingSVG redrawingSVG = new RedrawingSVG();
         redrawingSVG.drawSvg();
     }
 
-    public void drawSvg() throws MalformedURLException, InterruptedException, FileNotFoundException, UnsupportedEncodingException, SVGGraphics2DIOException {
+    public void drawSvg() throws MalformedURLException {
         final JSVGCanvas canvas = new JSVGCanvas();
         canvas.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC); // to update it
-        canvas.setURI(new File("/home/INTEXSOFT/eugene.kuntsevich/Downloads/img.svg").toURI().toURL().toString());
+        canvas.setURI(new File("/home/ekuntsevich/Downloads/img.svg").toURI().toURL().toString());
 
         canvas.addGVTTreeBuilderListener(new GVTTreeBuilderAdapter() {
             public void gvtBuildCompleted(GVTTreeBuilderEvent e) {
@@ -41,8 +48,7 @@ public class RedrawingSVG extends JFrame {
         });
         getContentPane().add(canvas);
 
-        setSize(800, 400);
-        setVisible(true);
+        setSize(400, 200);
 
         synchronized (shown) { // Strictly required to wait
             while (shown.get() == false) {
@@ -52,23 +58,21 @@ public class RedrawingSVG extends JFrame {
                 }
             }
         }
-        Document doc = canvas.getSVGDocument();
-        doc.setDocumentURI("http://www.w3.org/2000/svg");
+        Document document = canvas.getSVGDocument();
 
-        SVGGraphics2D g = new SVGGraphics2D(doc);
-        Shape circle = new Ellipse2D.Double(50, 50, 1000, 1000);
-        g.setPaint(Color.red);
-        g.setStroke(new BasicStroke(25));
-        g.draw(circle);
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        drawArrow(svgGenerator, 25);
+        DrawText drawText = new DrawText();
+        drawText.paint(svgGenerator);
 
-        Element root = doc.getDocumentElement();
-        g.getRoot(root);
+        Element root = document.getDocumentElement();
+        svgGenerator.getRoot(root);
 
-        /*Writer out;
+        Writer out;
         try {
             OutputStream outputStream = new FileOutputStream(new File("img2.svg"));
             out = new OutputStreamWriter(outputStream, "UTF-8");
-            g.stream(out, true);
+            svgGenerator.stream(root, out);
             outputStream.flush();
             outputStream.close();
         } catch (UnsupportedEncodingException e) {
@@ -80,6 +84,5 @@ public class RedrawingSVG extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-       // System.exit(0);*/
     }
 }
